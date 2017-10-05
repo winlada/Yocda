49d56b4dd12f342df2aedac3b30453567e434be4
Winlada <tottot.11112222@gmail.com>
2017-10-05 10:47:20 +0700

    sum

--- num.py
+++ num.py
@@ -1,3 +1,8 @@
-def main(num):
-	print(num)
-main(int(input()))
+def main(num, num2):
+	summ = summation(num, num2)
+	subb = subtraction(num, num2)
+	return subb, summ
+
+def summation(n1, n2):
+	return n1 + n2
+main(int(input()), int(input()))
